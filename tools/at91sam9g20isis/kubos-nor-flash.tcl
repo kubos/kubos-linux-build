7aa33296a9e6b5102db9535bc2272f80df185ac9
Catherine <catherine@kubos.co>
2017-04-13 15:40:42 -0500

    Adding NOR flashing script

---
+++ tools/at91sam9g20isis/kubos-nor-flash.tcl
@@ -0,0 +1,73 @@
+ #
+ # Copyright (C) 2017 Kubos Corporation
+ #
+ # Licensed under the Apache License, Version 2.0 (the "License");
+ # you may not use this file except in compliance with the License.
+ # You may obtain a copy of the License at
+ #
+ #     http://www.apache.org/licenses/LICENSE-2.0
+ #
+ # Unless required by applicable law or agreed to in writing, software
+ # distributed under the License is distributed on an "AS IS" BASIS,
+ # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ # See the License for the specific language governing permissions and
+ # limitations under the License.
+ #
+ # Flash KubOS Linux related NOR flash files on the at91sam9g20isis board
+ # using the SAM-BA flashing utility
+ #
+ # Note: The iOBC must be connected, powered, and not running KubOS Linux
+ #  in order for this script to work. (If board is currently running 
+ #  KubOS Linux, reboot the board and hold down any key to boot into the 
+ #  U-Boot CLI instead)
+ # 
+ # This script will be passed as an argument when calling the SAM-BA flashing
+ # utility:
+ #   $ {path to SAM-BA}/sam-ba.exe /jlink/ARM0 at91sam9g20-ISISOBC \
+ #        kubos-nor-flash.tcl {input arguments}
+ #
+ # Inputs:
+ #  * uboot={uboot file} - Path to U-Boot binary
+ #  * dtb={dtb file} - Path to Device Tree binary
+ #  * altos={alt file} - Path to alternate OS binary
+ # 
+
+puts "Running NOR flash script"
+ 
+if { $argc < 1 } {
+	puts "ERROR: Please specify at least one file to flash."}
+	return 1
+}
+
+# Init NOR Flash
+NORFLASH::Init
+
+foreach arg $::argv {
+
+	# Tokenize argument around '='
+	set fields [split $arg "="]
+	set param [lindex $fields 0]
+	set file [lindex $fields 1]
+
+	if {[string match "uboot" $param]}
+	{
+		set addr 0x0000A000
+	}
+	else if {[string match "dtb" $param]} 
+	{
+		set addr 0x00080000
+	}
+	else if {[string match "altos" $param]} 
+	{
+		set addr 0x00090000
+	}
+	
+	puts "Flashing $file to $addr"
+	
+	# Flash and verify
+	send_file {NorFlash} "$file" $addr
+	compare_file {NorFlash} "$file"  $addr
+
+}
+
+return 0
