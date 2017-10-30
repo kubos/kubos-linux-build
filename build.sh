0b25fb53d9571753602632f734f15d68445747f7
Catherine <catherine@kubos.co>
2017-10-30 09:24:58 -0500

    Build script should exit on error

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 buildroot_tar="buildroot-2016.11.tar.gz"
 buildroot_url="https://buildroot.uclibc.org/downloads/$buildroot_tar"
 
@@ -27,6 +29,13 @@ if [[ $board == "beaglebone-black" ]];
 then
     echo "Copying beaglebone sdimage to $CIRCLE_ARTIFACTS"
     cp ./output/images/kubos-linux.tar.gz $CIRCLE_ARTIFACTS/
+    /bin/bash ../kubos-linux-build/post.sh
+elif [[ $board == "at91sam9g20isis" ]];
+then
+    echo "Copying iOBC files to $CIRCL_ARTIFACTS"
+    cp ./output/images/kubos-linux.tar.gz $CIRCLE_ARTIFACTS/
+    cp ./output/images/u-boot.bin $CIRCLE_ARTIFACTS/
+    cp ./output/images/at91sam9g20isis.dtb $CIRCLE_ARTIFACTS/
     /bin/bash ../kubos-linux-build/post.sh
 else
     echo "The output artifacts are not configured for board \"$board\""
