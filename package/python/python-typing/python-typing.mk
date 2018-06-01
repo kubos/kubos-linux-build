f06a40a268bccb9327b0785535f7ef81f553a81d
Catherine Garabedian <catherine@kubos.co>
2018-06-01 15:37:34 +0000

    Upgrading python packages to use the latest version of graphene

--- package/python/python-typing/python-typing.mk
+++ package/python/python-typing/python-typing.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_TYPING_VERSION = 3.6.2
+PYTHON_TYPING_VERSION = 3.6.4
 PYTHON_TYPING_SOURCE = typing-$(PYTHON_TYPING_VERSION).tar.gz
-PYTHON_TYPING_SITE = https://pypi.python.org/packages/ca/38/16ba8d542e609997fdcd0214628421c971f8c395084085354b11ff4ac9c3
+PYTHON_TYPING_SITE = https://files.pythonhosted.org/packages/ec/cc/28444132a25c113149cec54618abc909596f0b272a74c55bab9593f8876c
 PYTHON_TYPING_SETUP_TYPE = setuptools
 PYTHON_TYPING_LICENSE = Python Software Foundation License
 PYTHON_TYPING_LICENSE_FILES = LICENSE
