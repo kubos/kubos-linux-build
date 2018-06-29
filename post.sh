2ede5186bedd9c8be45d0517d9b8f9bc07239f81
Catherine Garabedian <catherine@kubos.co>
2018-06-29 16:49:33 +0000

    Removing post-build-artifact-to-slack script

--- post.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-artifact_url="https://${CIRCLE_BUILD_NUM}-73321966-gh.circle-artifacts.com/0${CIRCLE_ARTIFACTS}/kubos-linux.tar.gz"
-curl --request POST --header "Content-Type: application/json" --data '{"text": "SD card image should be available at: '$artifact_url'"}' $ARTIFACT_WEBHOOK_URL
