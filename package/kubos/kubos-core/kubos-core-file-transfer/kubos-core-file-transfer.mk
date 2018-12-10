45f4f295b98d0d944ce196b66926c805005a00f4
Catherine Garabedian <catherine@kubos.co>
2018-12-10 20:48:02 +0000

    Adding monit and policies for Kubos core services

--- package/kubos/kubos-core/kubos-core-file-transfer/kubos-core-file-transfer.mk
+++ package/kubos/kubos-core/kubos-core-file-transfer/kubos-core-file-transfer.mk
@@ -19,6 +19,11 @@ define FILE_TRANSFER_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/usr/sbin
 	$(INSTALL) -D -m 0755 $(BUILD_DIR)/kubos-$(KUBOS_VERSION)/$(CARGO_OUTPUT_DIR)/file-service \
 		$(TARGET_DIR)/usr/sbin
+		
+    echo 'CHECK PROCESS file-service PIDFILE /var/run/file-service.pid' > $(TARGET_DIR)/etc/monit.d/kubos-app-service.cfg
+    echo '    START PROGRAM = "/etc/init.d/S${BR2_KUBOS_CORE_APP_SERVICE_INIT_LVL}kubos-core-file-transfer start"' >> $(TARGET_DIR)/etc/monit.d/kubos-app-service.cfg 
+    echo '    IF ${BR2_KUBOS_CORE_APP_SERVICE_RESTART_COUNT} RESTART WITHIN ${BR2_KUBOS_CORE_APP_SERVICE_RESTART_CYCLES} CYCLES THEN TIMEOUT' \
+    >> $(TARGET_DIR)/etc/monit.d/kubos-file-transfer.cfg  
 endef
 
 # Install the init script
